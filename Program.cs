e2829dfe52b5894889e434bc8e2b24fd27d094dd
mazn <imzn77@gmail.com>
2024-10-12 17:50:12 +0300

    .

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+public class m
+{
+   static void Main(string[]args)
+   {
+
+string str1;// First Number
+string str2;// 2st Number
+
+Console.WriteLine("Enter first num:");
+    str1 = Console.ReadLine();
+Console.WriteLine("Enter sec num:");
+    str2= Console.ReadLine();
+Console.WriteLine("Enter The operation:");
+   
+    
+    int num1=int.Parse(str1); //string to int
+    int num2=int.Parse(str2); //string to int
+Console.WriteLine($"{num1}+{num2}="+(num1+num2));// result: num1+num2=??
+Console.WriteLine($"{num1}-{num2}="+(num1-num2));// result: num1-num2=??    
+Console.WriteLine($"{num1}/{num2}="+(num1/num2));// result: num1/num2=??    
+Console.WriteLine($"{num1}*{num2}="+(num1*num2));// result: num1*num2=??    
+
+
+    
+   }
+}
