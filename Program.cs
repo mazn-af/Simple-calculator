df2e8e429716f6143023e03781e63c50e7a81b6c
mazn <160425952+mazn-af@users.noreply.github.com>
2024-10-17 00:34:13 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,42 +3,39 @@
 class Program
 {
    public static void Main()
-    {
+   {
         Console.WriteLine("Enter the first number:");
         double num1 = Convert.ToDouble(Console.ReadLine());
-
         Console.WriteLine("Enter the second number:");
         double num2 = Convert.ToDouble(Console.ReadLine());
-
         Console.Write("Enter the operation (+, -, *, /):");
         string operation = Console.ReadLine();
-
         double result = 0;
 
         if (operation == "+")
         {
-            result = num1 + num2;
-            Console.WriteLine($"\nResult: {num1} + {num2} = {result}");
+         result = num1 + num2;
+         Console.WriteLine($"\nResult: {num1} + {num2} = {result}");
         }
         else if (operation == "-")
         {
-            result = num1 - num2;
-            Console.WriteLine($"\nResult: {num1} - {num2} = {result}");
+         result = num1 - num2;
+         Console.WriteLine($"\nResult: {num1} - {num2} = {result}");
         }
         else if (operation == "*")
         {
-            result = num1 * num2;
-            Console.WriteLine($"\nResult: {num1} * {num2} = {result}");
+         result = num1 * num2;
+         Console.WriteLine($"\nResult: {num1} * {num2} = {result}");
         }
         else if (operation == "/")
         {
             
-                result = num1 / num2;
-                Console.WriteLine($"\nResult: {num1} / {num2} = {result}");
+         result = num1 / num2;
+         Console.WriteLine($"\nResult: {num1} / {num2} = {result}");
         }
         else
         {
-            Console.WriteLine("Invalid operation. Please enter +, -, *, /");
+         Console.WriteLine("Invalid operation. Please enter +, -, *, /");
         }
     }
 }
